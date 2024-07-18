65a4f498f7230dc5b177c64f2e4dc84bd42cac0b
ugochukwumaduakor <umaduakor@hotmail.com>
2024-07-19 01:01:53 +1000

    first code for rule 3

--- Dice Roller Game/Program.cs
+++ Dice Roller Game/Program.cs
@@ -7,7 +7,10 @@ class Program
     {
         Random dice = new Random();
         Console.WriteLine("*************************** Welcome To Rolling Dice ************************\n");
-
+        Console.WriteLine("GAME RULES" +
+            "\n1, If any two dice you roll result in the same value, you get two bonus points for rolling doubles." +
+            "\n2, If all three dice you roll result in the same value, you get six bonus points for rolling triples." +
+            "\n3, If the sum of the three dice rolls, plus any point bonuses, is 15 or greater, you win the game. Otherwise, you lose.\n");
         int roll1 = dice.Next(1,7);
         int roll2 = dice.Next(1, 7);
         int roll3 = dice.Next(1, 7);
@@ -15,6 +18,16 @@ class Program
         int total = roll1 + roll2 + roll3;
 
         Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+        if (total > 14)
+        {
+            Console.WriteLine("You win!");
+        }
+
+        if (total < 15)
+        {
+            Console.WriteLine("Sorry, you lose.");
+        }
     }
 }
 
